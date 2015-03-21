a99ef28c8dcbd8eed1410feb5611da08941387e7
rogersb11 <brettrogers11@gmail.com>
2015-03-21 18:29:11 -0400

    MORE G3 Cyanide goodness, whoa

--- cyanide.mk
+++ cyanide.mk
@@ -1,12 +1,12 @@
 $(call inherit-product, device/lge/vs985/full_vs985.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common CYANIDE stuff.
+$(call inherit-product, vendor/cyanide/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/cyanide/config/nfc_enhanced.mk)
 
-PRODUCT_NAME := cm_vs985
+PRODUCT_NAME := cyanide_vs985
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g3" \
